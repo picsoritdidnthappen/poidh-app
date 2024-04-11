d8c69fa3f77d080b9a2b852c7b92829c6ff5c5d6
rhovian <atomicjab@proton.me>
2024-04-11 11:24:12 -0500

    add degen chain & contracts

--- src/app/context/ContextProvider.tsx
+++ src/app/context/ContextProvider.tsx
@@ -20,9 +20,26 @@ const ContextProvider: React.FC<ContextProviderProps> = ({ children }) => {
     <DynamicContextProvider 
       settings={{ 
         environmentId: '622731b3-a151-4656-8b08-de858d71d397',
-        walletConnectors: [ EthereumWalletConnectors],
+        walletConnectors: [EthereumWalletConnectors],
         networkValidationMode: "always",
         walletConnectorExtensions: [EthersExtension],
+        evmNetworks: [
+          {
+            blockExplorerUrls: ['https://explorer.degen.tips'],
+            chainId: 666666666,
+            chainName: 'Degen Chain',
+            iconUrls: ['https://your-icon-url.com/degen-chain-icon.svg'],
+            name: 'Degen',
+            nativeCurrency: {
+              decimals: 18,
+              name: 'DEGEN',
+              symbol: 'DEGEN',
+            },
+            networkId: 666666666,
+            rpcUrls: ['https://rpc.degen.tips'],
+            vanityName: 'Degen Chain',
+          },
+        ]
       }}
       > 
      
--- src/app/context/config.ts
+++ src/app/context/config.ts
@@ -9,6 +9,14 @@ const chains = {
       nftContract: "0xd29F55255C784d777Aa7A09063E7D18377446fe2",
     },
   },
+  degen: {
+    name: "Degen Base Testnet",
+    jsonProviderUrl: "https://rpc.degen.tips",
+    contracts: {
+      mainContract: "0x0Aa50ce0d724cc28f8F7aF4630c32377B4d5c27d",
+      nftContract: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
+    },
+  },
 };
 
 
