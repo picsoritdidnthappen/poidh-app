ab513dd59fad20f10a3aea1345ef3a0d2bab2d8e
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2024-04-24 13:21:26 -0700

    Revert "Poidh v2 dev"

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,5 +3,3 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({ hello: 'Next.js' });
 }
-
-
--- src/lib/BalanceHook.ts
+++
@@ -1,8 +0,0 @@
-import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
-
-const { primaryWallet } = useDynamicContext();
-
-const  getBalance = async (primaryWallet: any) => {
-  const balance = await primaryWallet.connector.getBalance();
-  return balance;
-};
