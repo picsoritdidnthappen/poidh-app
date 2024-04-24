fca65ef36afd1a1ea62dbf4a3c2734dd36cb4e38
Gleb <88168209+hostgsr@users.noreply.github.com>
2024-04-24 22:43:35 +0200

    Update Menu.tsx

--- src/components/global/Menu.tsx
+++ src/components/global/Menu.tsx
@@ -13,7 +13,7 @@ const Menu: React.FC<MenuProps> = ({ menuPoints }) => {
 
 
   return (
-    <div className='flex items-center gap-x-5 flex-col lg:flex-row lg:absolute lg:left-1/2 lg:-translate-x-1/2'>
+    <div className='flex items-center gap-x-5 flex-col lg:flex-row lg:absolute lg:left-1/2 lg:translate-y-[-12px] lg:-translate-x-1/2'>
       <Link  href="https://info.poidh.xyz" >
         about us
       </Link>
