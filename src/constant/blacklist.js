6518d748593df00a62ea3b6598e53cc996b63dc3
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2024-04-29 22:11:05 -0700

    Update blacklist.js

--- src/constant/blacklist.js
+++ src/constant/blacklist.js
@@ -7,6 +7,10 @@ export const blacklist = [
     bountyId: 143,
     claims: [355,356,357],
   },
+  {
+    bountyId: 139,
+    claims: [370,371,414],
+  },
 ];
 
 export const blacklistedBounties = [160];
