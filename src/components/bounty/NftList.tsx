72e26cef521eb9536364f494657c147741ad5257
Chopper <defi.chopper@gmail.com>
2024-05-10 00:14:01 +0800

    feat: show degen or ens name on NFT list

--- src/components/bounty/NftList.tsx
+++ src/components/bounty/NftList.tsx
@@ -1,3 +1,4 @@
+import { useDegenOrEnsName } from '@/hooks/useDegenOrEnsName';
 import React from 'react';
 
 interface NFTDetail {
@@ -11,6 +12,10 @@ interface NftListProps {
   nftDetails: NFTDetail[] | null;  // Allow nftDetails to be null
 }
 
+interface NftListItemProps {
+  data: NFTDetail;
+}
+
 const NftList: React.FC<NftListProps> = ({ nftDetails }) => {
   if (nftDetails === null) {
     return <div className='text-center py-20'>no NFT details available.</div>;  // Handling the case where nftDetails is null
@@ -19,26 +24,32 @@ const NftList: React.FC<NftListProps> = ({ nftDetails }) => {
   return (
     <div className='md:grid flex flex-col md:grid-cols-8 lg:grid-cols-12 my-20 gap-12'>
       {nftDetails.map((detail, index) => (
-        <div className=' md:col-span-4 ' key={index}>
-             <div className='p-[2px] border text-white relative bg-[#F15E5F] border-[#F15E5F] border-2 rounded-xl '>
-
-        <div style={{backgroundImage: `url(${detail.uri})`}} className="bg-[#12AAFF] bg-cover bg-center w-full aspect-w-1 aspect-h-1 rounded-[8px] overflow-hidden">
-  
-</div>
-<div className="p-3">
-  <div className="flex break-words flex-col">
-  <p>{detail.name}</p>
-  <p className="">{detail.description}</p>
-  </div>
- 
-</div>
-</div> 
-
-         
-        </div>
+        <NftListItem data={detail} key={index} />
       ))}
     </div>
   );
+};
+
+const NftListItem: React.FC<NftListItemProps> = ({ data: detail }) => {
+  const degenOrEnsName = useDegenOrEnsName(detail.name);
+
+  return <div className=' md:col-span-4 '>
+  <div className='p-[2px] border text-white relative bg-[#F15E5F] border-[#F15E5F] border-2 rounded-xl '>
+
+<div style={{backgroundImage: `url(${detail.uri})`}} className="bg-[#12AAFF] bg-cover bg-center w-full aspect-w-1 aspect-h-1 rounded-[8px] overflow-hidden">
+
+</div>
+<div className="p-3">
+<div className="flex break-words flex-col">
+<p>{degenOrEnsName || detail.name}</p>
+<p className="">{detail.description}</p>
+</div>
+
+</div>
+</div> 
+
+
+</div>;
 };
 
 export default NftList;
