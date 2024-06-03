5fb7d05d651abdb9df635879a68fa49a2786dd11
Chopper <defi.chopper@gmail.com>
2024-06-03 16:23:54 +0800

    fix: set init state val to null in useDegenOrEnsName

--- src/hooks/useDegenOrEnsName.ts
+++ src/hooks/useDegenOrEnsName.ts
@@ -6,7 +6,7 @@ import { getDegenOrEnsName } from '@/app/context/web3';
  * @note we can replace this hook with useQuery from react-query if we decide to install the package and use it
  * */
 export const useDegenOrEnsName = (addr: string) => {
-  const [result, setResult] = useState<string | null>(addr);
+  const [result, setResult] = useState<string | null>(null);
 
   useEffect(() => {
     const cb = async () => {
