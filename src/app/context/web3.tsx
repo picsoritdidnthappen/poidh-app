0c932edfe88f769e7e74054fb8e84531632828d1
kt49771 <kent49771@gmail.com>
2024-06-06 01:52:35 +0800

    fix: bounty page broken error

--- src/app/context/web3.tsx
+++ src/app/context/web3.tsx
@@ -57,6 +57,7 @@ export const getContract = async (signer: any) => {
 export const getContractRead = async () => {
   const provider = await getProvider();
   const currentChain = chainStatusStore.currentChain;
+  console.log('getContractRead', currentChain.name);
   return new Contract(currentChain.contracts.mainContract, abi, provider);
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from 'next';
 import React from 'react';
 
 import '@/styles/globals.css';
@@ -9,38 +8,6 @@ import Header from '@/components/layout/Header';
 
 import ContextProvider from '@/app/context/ContextProvider';
 import WalletProvider from '@/app/context/WalletProvider';
-import { siteConfig } from '@/constant/config';
-
-const metadata: Metadata = {
-  metadataBase: new URL(siteConfig.url),
-  title: {
-    default: siteConfig.title,
-    template: `%s | ${siteConfig.title}`,
-  },
-  description: siteConfig.description,
-  robots: { index: true, follow: true },
-  icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
-  },
-  manifest: `/favicon/site.webmanifest`,
-  openGraph: {
-    url: siteConfig.url,
-    title: siteConfig.title,
-    description: siteConfig.description,
-    siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    type: 'website',
-    locale: 'en_US',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.title,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.jpg`],
-  },
-};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html>
