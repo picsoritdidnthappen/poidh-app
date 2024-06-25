1d69ac7f222424063df077d654de696b691288bc
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2024-06-25 16:37:41 -0700

    Revert "A tree frog/graph is back"

--- src/app/bounty/[id]/layout.tsx
+++ src/app/bounty/[id]/layout.tsx
@@ -4,40 +4,23 @@ import { fetchBountyById } from '@/app/context/web3';
 import '@/styles/colors.css';
 
 type Props = {
-  params: { id: string; netname: string };
+  params: { id: string };
 };
-
-function weiToEther(weiValue: string | number | bigint): string {
-  const etherValue = Number(weiValue) / 1e18;
-  return etherValue.toFixed(6);
-}
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // read route params
   const id = params.id;
-  let netName = params.netname;
-
-  if (
-    !netName ||
-    netName === '' ||
-    netName == 'arbitrum' ||
-    netName == 'base'
-  ) {
-    netName = 'eth';
-  }
 
   // fetch data
   const bountyData = await fetchBountyById(id);
 
   return {
     title: bountyData.name,
-    description:
-      weiToEther(bountyData.amount) + ` ${netName} ` + bountyData.description,
+    description: bountyData.description,
 
     openGraph: {
       title: bountyData.name,
-      description:
-        weiToEther(bountyData.amount) + ` ${netName} ` + bountyData.description,
+      description: bountyData.description,
       siteName: 'POIDH',
       images: [`https://poidh.xyz/images/poidh-preview-hero.png`],
       type: 'website',
@@ -46,8 +29,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     twitter: {
       card: 'summary_large_image',
       title: bountyData.name,
-      description:
-        weiToEther(bountyData.amount) + ` ${netName} ` + bountyData.description,
+      description: bountyData.description,
       images: [`https://poidh.xyz/images/poidh-preview-hero.png`],
     },
   };
