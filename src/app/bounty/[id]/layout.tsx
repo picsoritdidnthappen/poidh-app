91007ffe1a25c7edd2903aa3971dd01b4b482b6e
aTreeFrog <shyonbabazadeh@gmail.com>
2024-06-26 22:37:25 -0700

    for testing

--- src/app/bounty/[id]/layout.tsx
+++ src/app/bounty/[id]/layout.tsx
@@ -1,26 +1,57 @@
 import { Metadata } from 'next';
 import * as React from 'react';
 import { fetchBountyById } from '@/app/context/web3';
+import chainStatusStore from '@/store/chainStatus.store';
 import '@/styles/colors.css';
 
 type Props = {
-  params: { id: string };
+  params: { id: string; netname: string };
 };
+
+function weiToEther(weiValue: string | number | bigint): string {
+  const etherValue = Number(weiValue) / 1e18;
+  return etherValue.toFixed(6);
+}
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // read route params
   const id = params.id;
+  let token = params.netname;
+  let currency = 'degen';
+
+  let netName = 'base';
+  if (
+    token &&
+    (token === 'base' || token === 'degen' || token === 'arbitrum')
+  ) {
+    netName = token;
+  }
+
+  if (
+    !netName ||
+    netName === '' ||
+    netName == 'arbitrum' ||
+    netName == 'base'
+  ) {
+    currency = 'eth';
+  }
+
+  chainStatusStore.setCurrentChainFromNetwork(netName);
 
   // fetch data
   const bountyData = await fetchBountyById(id);
 
   return {
     title: bountyData.name,
-    description: bountyData.description,
+    description:
+      weiToEther(bountyData.amount) + ` ${currency} ` + bountyData.description,
 
     openGraph: {
       title: bountyData.name,
-      description: bountyData.description,
+      description:
+        weiToEther(bountyData.amount) +
+        ` ${currency} ` +
+        bountyData.description,
       siteName: 'POIDH',
       images: [`https://poidh.xyz/images/poidh-preview-hero.png`],
       type: 'website',
@@ -29,7 +60,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     twitter: {
       card: 'summary_large_image',
       title: bountyData.name,
-      description: bountyData.description,
+      description:
+        weiToEther(bountyData.amount) +
+        ` ${currency} ` +
+        bountyData.description,
       images: [`https://poidh.xyz/images/poidh-preview-hero.png`],
     },
   };
