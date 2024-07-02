651772111a6bbff4bd81e013c9bdd0649f50ee59
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-07-02 17:48:10 +0100

    feat: pr rollback

--- src/components/ui/ButtonCTA.tsx
+++ src/components/ui/ButtonCTA.tsx
@@ -3,7 +3,7 @@ const ButtonCTA = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <div
-        className={` flex cursor-pointer hover:bg-poidhRed active:bg-poidhRed  backdrop-blur-sm bg-[#D1ECFF]/20 	text-bold bg-gradient-to-t from-white-200 from-10%  via-30% to-50% gap-x-5  ${buttonClasses} `}
+        className={` flex backdrop-blur-sm bg-[#D1ECFF]/20 	text-bold bg-gradient-to-t from-white-200 from-10%  via-30% to-50% gap-x-5  ${buttonClasses} `}
       >
         {children}
       </div>
