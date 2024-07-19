857fa6b9631061d6701ce1d6f444361ccf1427ac
Ian Elliott <ie2173@columbia.edu>
2024-07-18 22:36:12 -0700

    eslint sort imports enabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 'use client';
 
 import * as React from 'react';
+import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
 
 import ContentHome from '@/components/layout/ContentHome';
 import CreateBounty from '@/components/ui/CreateBounty';
-import { Network } from 'ethers';
 import NetworkSelector from '@/components/ui/NetworkSelector';
-import { useState } from 'react';
-
 
 const Home = () => {
   const [showSections, setShowSections] = useState(false);
