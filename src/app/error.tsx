bae52083a6c667c9c085ee38b977ba5c05b782b7
gayatrigt <taleygayatri@gmail.com>
2025-02-22 17:50:10 -0700

    fix: 'removed header clause'

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -20,7 +20,7 @@ export default function Error({
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
           <WarrningIcon width={100} height={100} />
           <h1 className='mt-8 text-4xl md:text-6xl'>
-            Oops, something went wrong!
+            Oops, something went wrong! {error.message}
           </h1>
         </div>
       </section>
