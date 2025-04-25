b08026e562fa2a6427fb89564b2f8aad2c99ad12
whtsupbab3 <alina.lytovchenko20@gmail.com>
2025-04-25 13:09:35 -0700

    fix: issue #546

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -3,16 +3,17 @@
 import clientEnv from '@/utils/clientEnv';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { http } from 'viem';
-import { arbitrum, base, degen } from 'wagmi/chains';
+import { arbitrum, base, degen, mainnet } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'poidh',
   projectId: '784d6347a43d3f6e89f58b177f1b27f2',
-  chains: [degen, arbitrum, base],
+  chains: [mainnet, degen, arbitrum, base],
   transports: {
     [degen.id]: http(clientEnv.DEGEN_RPC_URL),
     [arbitrum.id]: http(clientEnv.ARBITRUM_RPC_URL),
     [base.id]: http(clientEnv.BASE_RPC_URL),
+    [mainnet.id]: http(clientEnv.BASE_RPC_URL),
   },
   ssr: true,
 });
