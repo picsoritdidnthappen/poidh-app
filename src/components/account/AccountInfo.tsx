5abff961ff30b2f6b1d9fd450807cf0bb3621752
whtsupbab3 <alina.lytovchenko20@gmail.com>
2025-04-30 11:54:28 -0700

    fix: poidh score loading view

--- src/components/account/AccountInfo.tsx
+++ src/components/account/AccountInfo.tsx
@@ -88,7 +88,7 @@ export default function AccountInfo({ address }: { address: string }) {
             <div className='mt-3 lg:mt-0 lg:ml-6 p-2 bg-white/5 rounded-lg backdrop-blur-sm text-center'>
               <div className='text-xs text-gray-300'>poidh score</div>
               <div className="text-4xl font-bold mt-1 text-poidhRed font-['PixeloidSans'] ">
-                {accountStats.isLoading ? '...' : accountStats.data?.poidhScore}
+                {accountStats.isLoading ? '…' : accountStats.data?.poidhScore}
               </div>
             </div>
           </div>
