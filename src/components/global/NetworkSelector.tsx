23945076ef1aac4d7eb1596c704906635a7ff277
whtsupbab3 <alina.lytovchenko20@gmail.com>
2025-06-19 00:44:11 -0400

    feat: issue #673

--- src/components/global/NetworkSelector.tsx
+++ src/components/global/NetworkSelector.tsx
@@ -4,27 +4,32 @@ import React from 'react';
 import { ArbitrumIcon, BaseIcon, DegenIcon } from '@/components/global/Icons';
 
 const networks = [
-  { href: '/arbitrum', Icon: ArbitrumIcon },
-  { href: '/base', Icon: BaseIcon },
-  { href: '/degen', Icon: DegenIcon },
+  { href: '/arbitrum', Icon: ArbitrumIcon, label: 'arbitrum bounties' },
+  { href: '/base', Icon: BaseIcon, label: 'base bounties' },
+  { href: '/degen', Icon: DegenIcon, label: 'degen bounties' },
 ];
 
 export function NetworkSelector({
   width = 24,
   height = 24,
 }: {
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
 }) {
   return (
-    <div className='flex flex-row gap-2'>
-      {networks.map(({ href, Icon }) => (
+    <div className='flex flex-col md:flex-row gap-6 items-center'>
+      {networks.map(({ href, Icon, label }) => (
         <Link
           key={href}
           href={href}
-          className='border-[#D1ECFF] border rounded-full backdrop-blur-sm bg-white/30 p-2 hover:bg-white/20'
+          className='flex flex-col items-center group'
         >
-          <Icon width={width} height={height} />
+          <span className='border-[#D1ECFF] border rounded-full backdrop-blur-sm bg-white/30 p-3 hover:bg-white/20 transition-colors'>
+            <Icon width={width} height={height} />
+          </span>
+          <h3 className='font-mono text-xl md:text-lg mt-3 underline group-hover:no-underline'>
+            {label}
+          </h3>
         </Link>
       ))}
     </div>
