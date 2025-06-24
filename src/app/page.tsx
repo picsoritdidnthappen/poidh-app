15b3e7a48f606258f0d2dc57d51a5da4702c4214
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2025-06-23 21:13:13 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,15 +29,15 @@ const Home = () => {
         <h3 className='font-mono text-2xl mt-8 mb-4 tracking-wide'>
           you can just incentivize things
         </h3>
-        <p className='text-lg mb-8'>step 1</p>
+        <p className='text-lg mb-8'></p>
         <h3 className='font-mono text-xl mb-6 tracking-wide'>
           fund a bounty 💰
         </h3>
-        <p className='text-lg mb-8'>step 2</p>
+        <p className='text-lg mb-8'></p>
         <h3 className='font-mono text-xl mb-6 tracking-wide'>
           share it 📢
         </h3>
-        <p className='text-lg mb-8'>step 3</p>
+        <p className='text-lg mb-8'></p>
         <h3 className='font-mono text-xl mb-6 tracking-wide'>
           approve it 🤝
         </h3>
