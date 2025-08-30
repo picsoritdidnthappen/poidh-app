63e1fd8fd8a22e1dffe716b6b6b937616e7e5036
whtsupbab3 <alina.lytovchenko20@gmail.com>
2025-08-30 14:43:31 -0400

    fix: blank album on explore page

--- src/components/albums/AlbumList.tsx
+++ src/components/albums/AlbumList.tsx
@@ -18,11 +18,6 @@ export default function AlbumList({ keyword = '' }: { keyword?: string }) {
     isFetching,
   } = trpc.albums.useQuery({ contains: debouncedKeyword });
 
-  // temprorary code for debugging
-  useEffect(() => {
-    console.log('albums:', albums);
-  }, [albums]);
-
   return (
     <div className='w-full max-w-sm mx-auto'>
       {!albums && !isLoading ? (
@@ -35,14 +30,16 @@ export default function AlbumList({ keyword = '' }: { keyword?: string }) {
         </div>
       ) : albums && albums.length > 0 ? (
         <div className='flex flex-col space-y-2'>
-          {albums.filter(Boolean).map((album, idx) => (
-            <Link href={`/a/${encodeURIComponent(album.album)}`} key={idx}>
-              <div className='flex justify-between items-center px-4 py-2 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl text-white font-mono transition-transform hover:shadow-xl hover:bg-white/30 hover:cursor-pointer hover:border-poidhRed'>
-                <span className='truncate'>{album.album}</span>
-                <span className='opacity-80'>{album._count?.album}</span>
-              </div>
-            </Link>
-          ))}
+          {albums
+            .filter((album) => album.album)
+            .map((album, idx) => (
+              <Link href={`/a/${encodeURIComponent(album.album)}`} key={idx}>
+                <div className='flex justify-between items-center px-4 py-2 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl text-white font-mono transition-transform hover:shadow-xl hover:bg-white/30 hover:cursor-pointer hover:border-poidhRed'>
+                  <span className='truncate'>{album.album}</span>
+                  <span className='opacity-80'>{album._count?.album}</span>
+                </div>
+              </Link>
+            ))}
         </div>
       ) : (
         <div className='text-center py-20 text-white/60'>
