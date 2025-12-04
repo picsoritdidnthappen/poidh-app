333ed2abcc9457d251fd6841e3f1a4f956bb737d
whtsupbab3 <whtsupbab3@gmail.com>
2025-12-03 19:38:10 -0500

    refactor: clean old blcklist code

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,3 @@
 export { default as ABI } from './abi/abi';
 export { default as DEGENNAMERESABI } from './abi/abiDegenNameRes';
 export { default as NFTABI } from './abi/abiNFT';
-export {
-  blacklist as Blacklist,
-  blacklistedBounties as BlacklistedBounties,
-} from './blacklist';
