8094a295e1f12c866e063ebd4d4af8698878626c
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2026-08-02 10:08:09 -0700

    Update web3.ts

--- src/trpc/routers/web3.ts
+++ src/trpc/routers/web3.ts
@@ -37,126 +37,130 @@ export const web3Router = {
       })
     )
     .query(async ({ input }) => {
-      const tenDays = 10 * 24 * 60 * 60 * 1000;
-      const tenDaysAgo = new Date(Date.now() - tenDays);
-      const user = await prisma.usersExtra.findFirst({
-        where: {
-          address: input.address.toLowerCase(),
-        },
-      });
-
-      if (!user || !user.gwei || user.lastUpdated < tenDaysAgo) {
-        try {
-          const gweiName = await mainnetPublicClient.readContract({
-            abi: GWEINAMESABI,
-            address: '0x9D51D507BC7264d4fE8Ad1cf7Fe191933A0a81d6',
-            functionName: 'reverseResolve',
-            args: [input.address as `0x${string}`],
-          });
-
-          if (gweiName) {
-            await prisma.usersExtra.upsert({
-              where: { address: input.address.toLowerCase() },
-              update: { gwei: gweiName, lastUpdated: new Date() },
-              create: {
-                address: input.address.toLowerCase(),
-                gwei: gweiName,
-                lastUpdated: new Date(),
-              },
-            });
-
-            return gweiName;
-          }
-        } catch {}
-      }
-
-      if (user?.gwei) {
-        return user.gwei;
-      }
-
-      if (!user || !user.wei || user.lastUpdated < tenDaysAgo) {
-        try {
-          const weiName = await mainnetPublicClient.readContract({
-            abi: WEINAMESABI,
-            address: '0x0000000000696760E15f265e828DB644A0c242EB',
-            functionName: 'reverseResolve',
-            args: [input.address as `0x${string}`],
-          });
-
-          if (weiName) {
-            await prisma.usersExtra.upsert({
-              where: { address: input.address.toLowerCase() },
-              update: { wei: weiName, lastUpdated: new Date() },
-              create: {
-                address: input.address.toLowerCase(),
-                wei: weiName,
-                lastUpdated: new Date(),
-              },
-            });
-
-            return weiName;
-          }
-        } catch {}
-      }
-
-      if (user?.wei) {
-        return user.wei;
-      }
-
-      if (!user || !user.ens || user.lastUpdated < tenDaysAgo) {
-        try {
-          const ensName = await mainnetPublicClient.getEnsName({
-            address: input.address as `0x${string}`,
-          });
-
-          if (ensName) {
-            await prisma.usersExtra.upsert({
-              where: { address: input.address.toLowerCase() },
-              update: { ens: ensName, lastUpdated: new Date() },
-              create: {
-                address: input.address.toLowerCase(),
-                ens: ensName,
-                lastUpdated: new Date(),
-              },
-            });
-
-            return ensName;
-          }
-        } catch {}
-      }
-
-      if (user?.ens) {
-        return user.ens;
-      }
-
-      if (!user || !user.degenName || user.lastUpdated < tenDaysAgo) {
-        try {
-          const degenName = await degenPublicClient.readContract({
-            abi: DEGENNAMERESABI,
-            address: '0x4087fb91A1fBdef05761C02714335D232a2Bf3a1',
-            functionName: 'defaultNames',
-            args: [input.address as `0x${string}`],
-          });
-
-          if (degenName) {
-            await prisma.usersExtra.upsert({
-              where: { address: input.address.toLowerCase() },
-              update: {
-                degenName: `${degenName}.degen`,
-                lastUpdated: new Date(),
-              },
-              create: {
-                address: input.address.toLowerCase(),
-                degenName: `${degenName}.degen`,
-                lastUpdated: new Date(),
-              },
-            });
-
-            return `${degenName}.degen`;
-          }
-        } catch {}
-      }
-
-      return user?.degenName ?? null;
+      return getHumanReadableName(input.address);
     }),
 };
+
+export async function getHumanReadableName(address: string) {
+  const tenDays = 10 * 24 * 60 * 60 * 1000;
+  const tenDaysAgo = new Date(Date.now() - tenDays);
+  const user = await prisma.usersExtra.findFirst({
+    where: {
+      address: address.toLowerCase(),
+    },
+  });
+
+  if (!user || !user.gwei || user.lastUpdated < tenDaysAgo) {
+    try {
+      const gweiName = await mainnetPublicClient.readContract({
+        abi: GWEINAMESABI,
+        address: '0x9D51D507BC7264d4fE8Ad1cf7Fe191933A0a81d6',
+        functionName: 'reverseResolve',
+        args: [address as `0x${string}`],
+      });
+
+      if (gweiName) {
+        await prisma.usersExtra.upsert({
+          where: { address: address.toLowerCase() },
+          update: { gwei: gweiName, lastUpdated: new Date() },
+          create: {
+            address: address.toLowerCase(),
+            gwei: gweiName,
+            lastUpdated: new Date(),
+          },
+        });
+
+        return gweiName;
+      }
+    } catch {}
+  }
+
+  if (user?.gwei) {
+    return user.gwei;
+  }
+
+  if (!user || !user.wei || user.lastUpdated < tenDaysAgo) {
+    try {
+      const weiName = await mainnetPublicClient.readContract({
+        abi: WEINAMESABI,
+        address: '0x0000000000696760E15f265e828DB644A0c242EB',
+        functionName: 'reverseResolve',
+        args: [address as `0x${string}`],
+      });
+
+      if (weiName) {
+        await prisma.usersExtra.upsert({
+          where: { address: address.toLowerCase() },
+          update: { wei: weiName, lastUpdated: new Date() },
+          create: {
+            address: address.toLowerCase(),
+            wei: weiName,
+            lastUpdated: new Date(),
+          },
+        });
+
+        return weiName;
+      }
+    } catch {}
+  }
+
+  if (user?.wei) {
+    return user.wei;
+  }
+
+  if (!user || !user.ens || user.lastUpdated < tenDaysAgo) {
+    try {
+      const ensName = await mainnetPublicClient.getEnsName({
+        address: address as `0x${string}`,
+      });
+
+      if (ensName) {
+        await prisma.usersExtra.upsert({
+          where: { address: address.toLowerCase() },
+          update: { ens: ensName, lastUpdated: new Date() },
+          create: {
+            address: address.toLowerCase(),
+            ens: ensName,
+            lastUpdated: new Date(),
+          },
+        });
+
+        return ensName;
+      }
+    } catch {}
+  }
+
+  if (user?.ens) {
+    return user.ens;
+  }
+
+  if (!user || !user.degenName || user.lastUpdated < tenDaysAgo) {
+    try {
+      const degenName = await degenPublicClient.readContract({
+        abi: DEGENNAMERESABI,
+        address: '0x4087fb91A1fBdef05761C02714335D232a2Bf3a1',
+        functionName: 'defaultNames',
+        args: [address as `0x${string}`],
+      });
+
+      if (degenName) {
+        await prisma.usersExtra.upsert({
+          where: { address: address.toLowerCase() },
+          update: {
+            degenName: `${degenName}.degen`,
+            lastUpdated: new Date(),
+          },
+          create: {
+            address: address.toLowerCase(),
+            degenName: `${degenName}.degen`,
+            lastUpdated: new Date(),
+          },
+        });
+
+        return `${degenName}.degen`;
+      }
+    } catch {}
+  }
+
+  return user?.degenName ?? null;
+}
