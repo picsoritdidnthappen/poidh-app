9bc0444aa1d08cd5ad2e214c4c7cecc780fec1e1
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2026-08-22 12:20:15 -0700

    Update claims.ts

--- src/trpc/routers/claims.ts
+++ src/trpc/routers/claims.ts
@@ -1,197 +1,39 @@
-import { z } from 'zod';
-import { baseProcedure } from '../init';
-import prisma from 'prisma/prisma';
-import axios from 'axios';
-import { tryCatchAsync } from '@/utils/utils';
-
-export const claimsRouter = {
-  fetch: baseProcedure
-    .input(z.object({ claimId: z.number(), chainId: z.number() }))
-    .query(async ({ input }) => {
-      const claim = await prisma.claims.findUniqueOrThrow({
-        where: {
-          id_chainId: {
-            id: input.claimId,
-            chainId: input.chainId,
-          },
-          ban: {
-            none: {},
-          },
+fetchBountyClaims: baseProcedure
+  .input(
+    z.object({
+      bountyId: z.number(),
+      chainId: z.number(),
+      limit: z.number().min(1).max(100).default(10),
+      cursor: z.number().nullish(),
+    })
+  )
+  .query(async ({ input }) => {
+    const items = await prisma.claims.findMany({
+      where: {
+        bountyId: input.bountyId,
+        chainId: input.chainId,
+        ban: {
+          none: {},
         },
-      });
+        ...(input.cursor
+          ? { isAccepted: false, id: { lt: input.cursor } }
+          : {}),
+      },
+      orderBy: [
+        !input.cursor ? { isAccepted: 'desc' } : {},
+        { id: 'desc' },
+      ],
+      take: input.limit,
+    });
 
-      const imageMetadata = await fetchImageMetadata(claim.url);
+    let nextCursor: number | undefined = undefined;
 
-      return {
-        ...claim,
-        url: imageMetadata.image,
-      };
-    }),
+    if (items.length === input.limit) {
+      nextCursor = items[items.length - 1].id;
+    }
 
-  fetchBountyClaims: baseProcedure
-    .input(
-      z.object({
-        bountyId: z.number(),
-        chainId: z.number(),
-        limit: z.number().min(1).max(100).default(10),
-        cursor: z.number().nullish(),
-      })
-    )
-    .query(async ({ input }) => {
-      const items = await prisma.claims.findMany({
-        where: {
-          bountyId: input.bountyId,
-          chainId: input.chainId,
-          ban: {
-            none: {},
-          },
-          ...(input.cursor
-            ? { isAccepted: false, id: { lt: input.cursor } }
-            : {}),
-        },
-        orderBy: [!input.cursor ? { isAccepted: 'desc' } : {}, { id: 'desc' }],
-        take: input.limit,
-      });
-
-      let nextCursor: number | undefined = undefined;
-      if (items.length === input.limit) {
-        nextCursor = items[items.length - 1].id;
-      }
-
-      return {
-        items,
-        nextCursor,
-      };
-
-  fetchAcceptedClaimByBountyId: baseProcedure
-    .input(z.object({ bountyId: z.number(), chainId: z.number() }))
-    .query(async ({ input }) => {
-      const claim = await prisma.claims.findFirst({
-        where: {
-          bountyId: input.bountyId,
-          chainId: input.chainId,
-          ban: {
-            none: {},
-          },
-          isAccepted: true,
-        },
-      });
-
-      if (!claim) {
-        return null;
-      }
-
-      const imageMetadata = await fetchImageMetadata(claim.url);
-
-      return {
-        ...claim,
-        url: imageMetadata.image,
-      };
-    }),
-
-  fetchVotingClaimByBountyId: baseProcedure
-    .input(z.object({ bountyId: z.number(), chainId: z.number() }))
-    .query(async ({ input }) => {
-      const vote = await prisma.votes.findFirst({
-        select: {
-          claimId: true,
-        },
-        where: {
-          ...input,
-          bounty: {
-            isVoting: true,
-          },
-        },
-        orderBy: { round: 'desc' },
-        take: 1,
-      });
-
-      if (!vote) {
-        return null;
-      }
-
-      const claim = await prisma.claims.findFirst({
-        where: {
-          id: vote.claimId,
-          chainId: input.chainId,
-          ban: {
-            none: {},
-          },
-        },
-      });
-
-      if (!claim) {
-        return null;
-      }
-
-      const imageMetadata = await fetchImageMetadata(claim.url);
-
-      return {
-        ...claim,
-        url: imageMetadata.image,
-      };
-    }),
-
-  isCreated: baseProcedure
-    .input(z.object({ chainId: z.number(), id: z.number() }))
-    .query(async ({ input }) => {
-      return prisma.claims.findUnique({
-        where: {
-          id_chainId: {
-            id: input.id,
-            chainId: input.chainId,
-          },
-        },
-      });
-    }),
-
-  isAccepted: baseProcedure
-    .input(z.object({ chainId: z.number(), id: z.number() }))
-    .query(async ({ input }) => {
-      return prisma.claims.findUnique({
-        where: {
-          id_chainId: {
-            id: input.id,
-            chainId: input.chainId,
-          },
-          isAccepted: true,
-        },
-      });
-    }),
-};
-
-export async function fetchImageMetadata(url: string) {
-  const [response, _] = await tryCatchAsync(async () => axios.get(url));
-
-  if (!response?.data) {
     return {
-      name: null,
-      description: null,
-      external_url: null,
-      image: null,
-      attributes: null,
+      items,
+      nextCursor,
     };
-  }
-
-  const responseSchema = z.object({
-    name: z.string(),
-    description: z.string(),
-    external_url: z.string(),
-    image: z.string(),
-    attributes: z.array(z.any()),
-  });
-
-  const parsed = responseSchema.safeParse(response.data);
-
-  if (!parsed.data) {
-    return {
-      name: null,
-      description: null,
-      external_url: null,
-      image: null,
-      attributes: null,
-    };
-  }
-
-  return parsed.data;
-}
+  }),
