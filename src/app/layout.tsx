b09f6eee55fc21cee20d96053d0f6034260de9df
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2026-09-11 10:13:57 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { LoadingLayout } from '@/components/global/LoadingLayout';
 import ClientLayout from '@/app/layout.client';
 import { Metadata } from 'next';
 import CryptoWalletMobilePopup from '@/components/global/CryptoWalletMobilePopup';
+import Footer from '@/components/global/Footer';
 import KonamiCamera from '@/components/global/KonamiCamera';
 import { Analytics } from '@vercel/analytics/next';
 import { ThemeProvider } from '@/context/ThemeContext';
