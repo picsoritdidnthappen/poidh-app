70e00927d8577048cb56e97afca30693ddaaa7bb
picsoritdidnthappen <152807131+picsoritdidnthappen@users.noreply.github.com>
2026-09-12 15:56:14 -0700

    Update route.ts

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -21,7 +21,7 @@ export async function GET() {
       name: 'poidh',
       iconUrl: 'https://poidh.xyz/icon.png',
       splashImageUrl: 'https://poidh.xyz/mini-splash-v2.png',
-      splashBackgroundColor: '#6fa9e1',
+      splashBackgroundColor: '#6faae1',
       homeUrl: 'https://poidh.xyz/',
       heroImageUrl: 'https://poidh.xyz/images/poidh-preview-hero-v2.png',
       webhookUrl: `https://api.neynar.com/f/app/${process.env.NEYNAR_CLIENT_ID}/event`,
